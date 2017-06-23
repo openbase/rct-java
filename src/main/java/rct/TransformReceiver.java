cd829eaef8ed1aecd7d1c21b5b1a86b979e34f3e
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2017-06-23 13:12:19 +0200

    requestTransform now throws a TransformerException

--- src/main/java/rct/TransformReceiver.java
+++ src/main/java/rct/TransformReceiver.java
@@ -13,144 +13,144 @@ import rct.impl.TransformerCore;
  * Creation of the frame tree creates overhead and should not be done on a
  * regular basis. Instead the transformer should exist for a longer period while
  * updating itself when changes to the frame tree occur.
- * 
+ *
  * @author lziegler
  *
  */
 public class TransformReceiver {
 
-	private TransformerCore core;
-	private TransformerConfig conf;
-	
-	@SuppressWarnings("unused")
-	private TransformCommunicator comm;
+    private TransformerCore core;
+    private TransformerConfig conf;
 
-	/**
-	 * Creates a new transformer. Attention: This should not be called by the
-	 * user, use {@link TransformerFactory} in order to create a transformer.
-	 * 
-	 * @param core
-	 *            The core functionality implementation
-	 * @param comm
-	 *            The communicator implementation
-	 * @param conf
-	 *            The configuration
-	 */
-	public TransformReceiver(TransformerCore core, TransformCommunicator comm,
-			TransformerConfig conf) {
-		this.core = core;
-		this.conf = conf;
-		this.comm = comm;
-	}
+    @SuppressWarnings("unused")
+    private TransformCommunicator comm;
 
-	/**
-	 * @brief Get the transform between two frames by frame ID.
-	 * @param target_frame
-	 *            The frame to which data should be transformed
-	 * @param source_frame
-	 *            The frame where the data originated
-	 * @param time
-	 *            The time at which the value of the transform is desired. (0
-	 *            will get the latest)
-	 * @return The transform between the frames
-	 * @throws TransformerException
-	 *
-	 */
-	public Transform lookupTransform(String target_frame, String source_frame,
-			long time) throws TransformerException {
-		return core.lookupTransform(target_frame, source_frame, time);
-	}
+    /**
+     * Creates a new transformer. Attention: This should not be called by the
+     * user, use {@link TransformerFactory} in order to create a transformer.
+     *
+     * @param core
+     * The core functionality implementation
+     * @param comm
+     * The communicator implementation
+     * @param conf
+     * The configuration
+     */
+    public TransformReceiver(TransformerCore core, TransformCommunicator comm,
+            TransformerConfig conf) {
+        this.core = core;
+        this.conf = conf;
+        this.comm = comm;
+    }
 
-	/**
-	 * @brief Get the transform between two frames by frame ID assuming fixed
-	 *        frame.
-	 * @param target_frame
-	 *            The frame to which data should be transformed
-	 * @param target_time
-	 *            The time to which the data should be transformed. (0 will get
-	 *            the latest)
-	 * @param source_frame
-	 *            The frame where the data originated
-	 * @param source_time
-	 *            The time at which the source_frame should be evaluated. (0
-	 *            will get the latest)
-	 * @param fixed_frame
-	 *            The frame in which to assume the transform is constant in
-	 *            time.
-	 * @return The transform between the frames
-	 * @throws TransformerException
-	 */
-	public Transform lookupTransform(String target_frame, long target_time,
-			String source_frame, long source_time, String fixed_frame)
-			throws TransformerException {
-		return core.lookupTransform(target_frame, target_time, source_frame,
-				source_time, fixed_frame);
-	}
+    /**
+     * @brief Get the transform between two frames by frame ID.
+     * @param target_frame
+     * The frame to which data should be transformed
+     * @param source_frame
+     * The frame where the data originated
+     * @param time
+     * The time at which the value of the transform is desired. (0
+     * will get the latest)
+     * @return The transform between the frames
+     * @throws TransformerException
+     *
+     */
+    public Transform lookupTransform(String target_frame, String source_frame,
+            long time) throws TransformerException {
+        return core.lookupTransform(target_frame, source_frame, time);
+    }
 
-	/**
-	 * @brief Request the transform between two frames by frame ID.
-	 * @param target_frame
-	 *            The frame to which data should be transformed
-	 * @param source_frame
-	 *            The frame where the data originated
-	 * @param time
-	 *            The time at which the value of the transform is desired. (0
-	 *            will get the latest)
-	 * @return A future object representing the request status and transform
-	 *         between the frames
-	 *
-	 */
-	public Future<Transform> requestTransform(String target_frame,
-			String source_frame, long time) {
-		return core.requestTransform(target_frame, source_frame, time);
-	}
+    /**
+     * @brief Get the transform between two frames by frame ID assuming fixed
+     * frame.
+     * @param target_frame
+     * The frame to which data should be transformed
+     * @param target_time
+     * The time to which the data should be transformed. (0 will get
+     * the latest)
+     * @param source_frame
+     * The frame where the data originated
+     * @param source_time
+     * The time at which the source_frame should be evaluated. (0
+     * will get the latest)
+     * @param fixed_frame
+     * The frame in which to assume the transform is constant in
+     * time.
+     * @return The transform between the frames
+     * @throws TransformerException
+     */
+    public Transform lookupTransform(String target_frame, long target_time,
+            String source_frame, long source_time, String fixed_frame)
+            throws TransformerException {
+        return core.lookupTransform(target_frame, target_time, source_frame,
+                source_time, fixed_frame);
+    }
 
-	/**
-	 * @brief Test if a transform is possible
-	 * @param target_frame
-	 *            The frame into which to transform
-	 * @param source_frame
-	 *            The frame from which to transform
-	 * @param time
-	 *            The time at which to transform
-	 * @param error_msg
-	 *            A pointer to a string which will be filled with why the
-	 *            transform failed, if not NULL
-	 * @return True if the transform is possible, false otherwise
-	 */
-	public boolean canTransform(String target_frame, String source_frame,
-			long time) {
-		return core.canTransform(target_frame, source_frame, time);
-	}
+    /**
+     * @throws rct.TransformerException
+     * @brief Request the transform between two frames by frame ID.
+     * @param targetFrame
+     * The frame to which data should be transformed
+     * @param sourceFrame
+     * The frame where the data originated
+     * @param time
+     * The time at which the value of the transform is desired. (0
+     * will get the latest)
+     * @return A future object representing the request status and transform
+     * between the frames
+     *
+     */
+    public Future<Transform> requestTransform(String targetFrame, String sourceFrame, long time) throws TransformerException {
+        return core.requestTransform(targetFrame, sourceFrame, time);
+    }
 
-	/**
-	 * @brief Test if a transform is possible
-	 * @param target_frame
-	 *            The frame into which to transform
-	 * @param target_time
-	 *            The time into which to transform
-	 * @param source_frame
-	 *            The frame from which to transform
-	 * @param source_time
-	 *            The time from which to transform
-	 * @param fixed_frame
-	 *            The frame in which to treat the transform as ant in time
-	 * @return True if the transform is possible, false otherwise
-	 */
-	public boolean canTransform(String target_frame, long target_time,
-			String source_frame, long source_time, String fixed_frame) {
-		return core.canTransform(target_frame, target_time, source_frame,
-				source_time, fixed_frame);
-	}
+    /**
+     * @brief Test if a transform is possible
+     * @param target_frame
+     * The frame into which to transform
+     * @param source_frame
+     * The frame from which to transform
+     * @param time
+     * The time at which to transform
+     * @param error_msg
+     * A pointer to a string which will be filled with why the
+     * transform failed, if not NULL
+     * @return True if the transform is possible, false otherwise
+     */
+    public boolean canTransform(String target_frame, String source_frame,
+            long time) {
+        return core.canTransform(target_frame, source_frame, time);
+    }
 
-	public TransformerConfig getConfig() {
-		return conf;
-	}
-    
-       /** 
-       * Shutdown the transform communicator
-       */
-       public void shutdown() {
-              comm.shutdown();
-       }
+    /**
+     * @brief Test if a transform is possible
+     * @param target_frame
+     * The frame into which to transform
+     * @param target_time
+     * The time into which to transform
+     * @param source_frame
+     * The frame from which to transform
+     * @param source_time
+     * The time from which to transform
+     * @param fixed_frame
+     * The frame in which to treat the transform as ant in time
+     * @return True if the transform is possible, false otherwise
+     */
+    public boolean canTransform(String target_frame, long target_time,
+            String source_frame, long source_time, String fixed_frame) {
+        return core.canTransform(target_frame, target_time, source_frame,
+                source_time, fixed_frame);
+    }
+
+    public TransformerConfig getConfig() {
+        return conf;
+    }
+
+    /**
+     * Shutdown the transform communicator
+     */
+    public void shutdown() {
+        comm.shutdown();
+    }
 }
--- src/main/java/rct/impl/TransformerCore.java
+++ src/main/java/rct/impl/TransformerCore.java
@@ -8,150 +8,154 @@ import rct.TransformerException;
 
 public interface TransformerCore extends TransformListener {
 
-	/** Clear all data */
-	void clear();
+    /**
+     * Clear all data
+     */
+    void clear();
 
-	/**
-	 * Add transform information to the rct data structure
-	 * 
-	 * @param transform
-	 *            The transform to store
-	 * @param authority
-	 *            The source of the information for this transform
-	 * @param is_static
-	 *            Record this transform as a static transform. It will be good
-	 *            across all time. (This cannot be changed after the first
-	 *            call.)
-	 * @return True unless an error occured
-	 * @throws TransformerException 
-	 */
-	boolean setTransform(Transform transform, boolean is_static) throws TransformerException;
+    /**
+     * Add transform information to the rct data structure
+     *
+     * @param transform
+     * The transform to store
+     * @param authority
+     * The source of the information for this transform
+     * @param is_static
+     * Record this transform as a static transform. It will be good
+     * across all time. (This cannot be changed after the first
+     * call.)
+     * @return True unless an error occured
+     * @throws TransformerException
+     */
+    boolean setTransform(Transform transform, boolean is_static) throws TransformerException;
 
-	/**
-	 * Get the transform between two frames by frame ID.
-	 * 
-	 * @param target_frame
-	 *            The frame to which data should be transformed
-	 * @param source_frame
-	 *            The frame where the data originated
-	 * @param time
-	 *            The time at which the value of the transform is desired. (0
-	 *            will get the latest)
-	 * @return The transform between the frames
-	 * @throws TransformerException 
-	 *
-	 */
-	Transform lookupTransform(String target_frame, String source_frame,
-			long time) throws TransformerException;
+    /**
+     * Get the transform between two frames by frame ID.
+     *
+     * @param target_frame
+     * The frame to which data should be transformed
+     * @param source_frame
+     * The frame where the data originated
+     * @param time
+     * The time at which the value of the transform is desired. (0
+     * will get the latest)
+     * @return The transform between the frames
+     * @throws TransformerException
+     *
+     */
+    Transform lookupTransform(String target_frame, String source_frame,
+            long time) throws TransformerException;
 
-	/**
-	 * Get the transform between two frames by frame ID assuming fixed frame.
-	 * 
-	 * @param target_frame
-	 *            The frame to which data should be transformed
-	 * @param target_time
-	 *            The time to which the data should be transformed. (0 will get
-	 *            the latest)
-	 * @param source_frame
-	 *            The frame where the data originated
-	 * @param source_time
-	 *            The time at which the source_frame should be evaluated. (0
-	 *            will get the latest)
-	 * @param fixed_frame
-	 *            The frame in which to assume the transform is ant in time.
-	 * @return The transform between the frames
-	 * @throws TransformerException 
-	 *
-	 */
-	Transform lookupTransform(String target_frame, long target_time,
-			String source_frame, long source_time, String fixed_frame) throws TransformerException;
+    /**
+     * Get the transform between two frames by frame ID assuming fixed frame.
+     *
+     * @param target_frame
+     * The frame to which data should be transformed
+     * @param target_time
+     * The time to which the data should be transformed. (0 will get
+     * the latest)
+     * @param source_frame
+     * The frame where the data originated
+     * @param source_time
+     * The time at which the source_frame should be evaluated. (0
+     * will get the latest)
+     * @param fixed_frame
+     * The frame in which to assume the transform is ant in time.
+     * @return The transform between the frames
+     * @throws TransformerException
+     *
+     */
+    Transform lookupTransform(String target_frame, long target_time,
+            String source_frame, long source_time, String fixed_frame) throws TransformerException;
 
-	/**
-	 * Request the transform between two frames by frame ID.
-	 * 
-	 * @param target_frame
-	 *            The frame to which data should be transformed
-	 * @param source_frame
-	 *            The frame where the data originated
-	 * @param time
-	 *            The time at which the value of the transform is desired. (0
-	 *            will get the latest)
-	 * @return A future object representing the request status and transform
-	 *         between the frames
-	 *
-	 */
-	Future<Transform> requestTransform(String target_frame,
-			String source_frame, long time);
+    /**
+     * Request the transform between two frames by frame ID.
+     *
+     * @param target_frame
+     * The frame to which data should be transformed
+     * @param source_frame
+     * The frame where the data originated
+     * @param time
+     * The time at which the value of the transform is desired. (0
+     * will get the latest)
+     * @return A future object representing the request status and transform
+     * between the frames
+     * @throws rct.TransformerException
+     *
+     */
+    Future<Transform> requestTransform(String target_frame, String source_frame, long time) throws TransformerException;
 
-	/**
-	 * Test if a transform is possible
-	 * 
-	 * @param target_frame
-	 *            The frame into which to transform
-	 * @param source_frame
-	 *            The frame from which to transform
-	 * @param time
-	 *            The time at which to transform
-	 * @param error_msg
-	 *            A pointer to a string which will be filled with why the
-	 *            transform failed, if not NULL
-	 * @return True if the transform is possible, false otherwise
-	 */
-	boolean canTransform(String target_frame, String source_frame, long time);
+    /**
+     * Test if a transform is possible
+     *
+     * @param target_frame
+     * The frame into which to transform
+     * @param source_frame
+     * The frame from which to transform
+     * @param time
+     * The time at which to transform
+     * @param error_msg
+     * A pointer to a string which will be filled with why the
+     * transform failed, if not NULL
+     * @return True if the transform is possible, false otherwise
+     */
+    boolean canTransform(String target_frame, String source_frame, long time);
 
-	/**
-	 * Test if a transform is possible
-	 * 
-	 * @param target_frame
-	 *            The frame into which to transform
-	 * @param target_time
-	 *            The time into which to transform
-	 * @param source_frame
-	 *            The frame from which to transform
-	 * @param source_time
-	 *            The time from which to transform
-	 * @param fixed_frame
-	 *            The frame in which to treat the transform as ant in time
-	 * @param error_msg
-	 *            A pointer to a string which will be filled with why the
-	 *            transform failed, if not NULL
-	 * @return True if the transform is possible, false otherwise
-	 */
-	boolean canTransform(String target_frame, long target_time,
-			String source_frame, long source_time, String fixed_frame);
+    /**
+     * Test if a transform is possible
+     *
+     * @param target_frame
+     * The frame into which to transform
+     * @param target_time
+     * The time into which to transform
+     * @param source_frame
+     * The frame from which to transform
+     * @param source_time
+     * The time from which to transform
+     * @param fixed_frame
+     * The frame in which to treat the transform as ant in time
+     * @param error_msg
+     * A pointer to a string which will be filled with why the
+     * transform failed, if not NULL
+     * @return True if the transform is possible, false otherwise
+     */
+    boolean canTransform(String target_frame, long target_time,
+            String source_frame, long source_time, String fixed_frame);
 
-	/** A way to get a std::vector of available frame ids */
-	Set<String> getFrameStrings();
+    /**
+     * A way to get a std::vector of available frame ids
+     */
+    Set<String> getFrameStrings();
 
-	/**
-	 * @brief Check if a frame exists in the tree
-	 * @param frame_id_str
-	 *            The frame id in question
-	 */
-	boolean frameExists(String frame_id_str);
+    /**
+     * @brief Check if a frame exists in the tree
+     * @param frame_id_str
+     * The frame id in question
+     */
+    boolean frameExists(String frame_id_str);
 
-	/**
-	 * @brief Fill the parent of a frame.
-	 * @param frame_id
-	 *            The frame id of the frame in question
-	 * @throws TransformerException 
-	 */
-	String getParent(String frame_id, long time) throws TransformerException;
+    /**
+     * @brief Fill the parent of a frame.
+     * @param frame_id
+     * The frame id of the frame in question
+     * @throws TransformerException
+     */
+    String getParent(String frame_id, long time) throws TransformerException;
 
-	/**
-	 * Backwards compatability. A way to see what frames have been cached Useful
-	 * for debugging
-	 */
-	String allFramesAsDot();
+    /**
+     * Backwards compatability. A way to see what frames have been cached Useful
+     * for debugging
+     */
+    String allFramesAsDot();
 
-	/**
-	 * A way to see what frames have been cached in yaml format Useful for
-	 * debugging tools
-	 */
-	String allFramesAsYAML();
+    /**
+     * A way to see what frames have been cached in yaml format Useful for
+     * debugging tools
+     */
+    String allFramesAsYAML();
 
-	/**
-	 * A way to see what frames have been cached Useful for debugging
-	 */
-	String allFramesAsString();
+    /**
+     * A way to see what frames have been cached Useful for debugging
+     */
+    String allFramesAsString();
 }
