953df494f440598ade1a19067521ce74bd5cc8a8
Divine Threepwood <divine@openbase.org>
2020-01-03 01:00:40 +0100

    Fix and improve synchronization future handling.

--- src/main/java/org/openbase/rct/impl/TransformRequest.java
+++ src/main/java/org/openbase/rct/impl/TransformRequest.java
@@ -28,6 +28,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import org.openbase.jul.schedule.SyncObject;
 import org.openbase.rct.Transform;
 
 public class TransformRequest {
@@ -52,66 +53,74 @@ public class TransformRequest {
     static public class FutureTransform implements Future<Transform> {
 
         private Transform transform = null;
-        private final Object lock = new Object();
+        private final Object lock = new SyncObject("FutureTransform");
         private boolean cancelled = false;
+        private boolean interrupt = false;
 
         @Override
         public boolean cancel(boolean mayInterruptIfRunning) {
             synchronized (lock) {
                 cancelled = true;
                 if (mayInterruptIfRunning) {
-                    lock.notify();
-                    return true;
+                    interrupt = true;
                 }
-                return false;
+                lock.notifyAll();
+                return true;
             }
         }
 
         public void set(Transform t) {
             synchronized (lock) {
-                if (!cancelled) {
-                    transform = t;
-                    lock.notify();
-                }
+                transform = t;
+                lock.notifyAll();
             }
         }
 
         @Override
         public Transform get() throws InterruptedException, ExecutionException {
             synchronized (lock) {
+
+                if (transform != null) {
+                    return transform;
+                }
+
                 if (cancelled) {
                     throw new CancellationException();
                 }
-                if (transform != null) {
-                    return transform;
-                } else {
-                    lock.wait();
-                    if (cancelled) {
-                        throw new CancellationException();
-                    }
-                    return transform;
+
+                lock.wait();
+
+                if (cancelled) {
+                    throw new CancellationException();
                 }
+
+                return transform;
             }
         }
 
         @Override
         public Transform get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
             synchronized (lock) {
+
+                if (transform != null) {
+                    return transform;
+                }
+
                 if (cancelled) {
                     throw new CancellationException();
                 }
-                if (transform != null) {
-                    return transform;
-                } else {
-                    lock.wait(TimeUnit.MILLISECONDS.convert(timeout, unit));
-                    if (cancelled) {
-                        throw new CancellationException();
-                    }
-                    if (transform == null) {
-                        throw new TimeoutException();
-                    }
-                    return transform;
+
+                lock.wait(TimeUnit.MILLISECONDS.convert(timeout, unit));
+
+                if (cancelled) {
+                    throw new CancellationException();
                 }
+
+                if (transform == null) {
+                    throw new TimeoutException();
+                }
+
+                return transform;
             }
         }
 
